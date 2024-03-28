0d33e07b7ff0730aa75225558955e374de2d8e83
deborah <fabbydebby@gmail.com>
2024-03-28 12:38:55 +0300

    array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  const lowerCaseName = name.toLowerCase();
+  return drivers.filter((driver) => driver.toLowerCase() === lowerCaseName);
+}
+
+function fuzzyMatch(drivers, query) {
+  const lowerCaseQuery = query.toLowerCase();
+  return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(lowerCaseQuery)
+  );
+}
+
+function matchName(drivers, name) {
+  return drivers.filter((driver) => driver.name === name);
+}
